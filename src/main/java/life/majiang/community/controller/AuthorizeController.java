18fe7e2c6fcbb1e6b9a1132b88cc8079fd3423e2
liangsigua <514102742@163.com>
2019-10-11 16:57:03 +0800

    use CookieAndSession style modified code

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -12,7 +12,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 //接受Github回调的地址，获取地址里的code和state
@@ -35,7 +36,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request){
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         //设置access_tokenr接口参数的值
         accessTokenDTO.setClient_id(clientId);
@@ -46,16 +47,19 @@ public class AuthorizeController {
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(accessToken);
 
-        //System.out.println(user.getName());
-        if (githubUser != null){
+        if (githubUser != null){      //验证成功
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            //这里把token抽取出来，代替session，放到cookie里
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
-            userMapper.insert(user);
-            request.getSession().setAttribute("user", githubUser);
+            userMapper.insert(user);  //就相当于写入session，用数据库的实物存储代替了写入session
+            //把token放到cookie里，目的就是往前端去写一个我们定义的cookie
+            response.addCookie(new Cookie("token",token));
+//            request.getSession().setAttribute("user", githubUser);
             return "redirect:/";
         }else{
             return "redirect:/";
--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -1,14 +1,38 @@
 package life.majiang.community.controller;
 
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        //当用户再次登陆的时候，根据cookie里的token去数据库里查询是否有这个用户，有就把这个用户写入到session中
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null){
+            for(Cookie cookie: cookies){
+                if (cookie.getName().equals("token")){
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);   //如果数据库里没有这个User，还可以加入返回的校验信息
+                    if (user != null){
+                        //把user写入到session里，这样子前端就能通过user在页面右上角展示用户名信息还是登陆按钮
+                        request.getSession().setAttribute("user",user);
+                    }
+                    break;
+                }
+            }
+        }
         return "index";
     }
 }
