de248a4605f353f8e6f01d5f4ea992300f5b8d0d
liangsigua <514102742@163.com>
2020-01-20 15:04:11 +0800

    use schedule

---
+++ src/main/java/life/majiang/community/Schedule/HotTagTasks.java
@@ -0,0 +1,20 @@
+package life.majiang.community.Schedule;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class HotTagTasks {
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        log.info("The time is now {}", dateFormat.format(new Date()));
+    }
+}
